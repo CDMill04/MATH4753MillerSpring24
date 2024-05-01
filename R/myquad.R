326ccde17e931b792672fa38db201ab769a43567
Caleb Miller <cdmill@Calebs-MacBook-Pro-6.local>
2024-05-01 16:52:40 -0500

    Initial Commitment

---
+++ R/myquad.R
@@ -0,0 +1,12 @@
+#' Quadratic Function
+#'
+#' @param x numeric vector
+#'
+#' @return numeric vector
+#' @export
+#'
+#' @examples
+#' myquad(x = 1:10)
+myquad = function(x) {
+  x^2 - 5*x + 6
+  }
