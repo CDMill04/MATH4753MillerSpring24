e0d023045c6034716bebd9f5551a4d9ec4f6119c
Caleb Miller <cdmill@Calebs-MacBook-Pro-6.local>
2024-05-02 16:22:34 -0500

    Adding FIREDAM dataset

---
+++ R/fire.R
@@ -0,0 +1,15 @@
+#' Fire Damage and Distance data
+#'
+#' A set of data detailing the damage caused by fires and the distance from the fire station.
+#'
+#'
+#' @format ## `fire`
+#' A data frame with 15 rows and 2 columns:
+#' \describe{
+#' \item{DISTANCE}{the distance from the fires station where this data was gathered, in miles}
+#' \item{DAMAGE}{X 1000 dollars in damages caused by the fires}
+#' ...
+#' }
+#' @source \url{https://www.crcpress.com/Statistics-for-Engineering-and-the-Sciences/Mendenhall-Sincich/p/book/9781498728850}
+#'
+"fire"
